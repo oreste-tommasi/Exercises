9a98f33833d9cd0044dfc86f4555325bf28c72cc
matteo <matteo@1728cb99-2769-0410-ae09-e727e7b20653>
2009-05-05 11:23:59 +0000

    * Implemented Exercise_1

---
+++ trunk/Exercise_1/main.cpp
@@ -0,0 +1,16 @@
+// system includes
+#include <iostream>
+
+// global namespace declaration
+using namespace std;
+
+
+int main ()
+{
+	cout << "Hello World\n";
+	
+	char a;
+	cin >> a;
+
+	return 0;
+}
