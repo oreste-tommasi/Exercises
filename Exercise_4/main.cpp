0bff27593288e1c087eb27be62f2bf93bfc73132
emiliano <emiliano@1728cb99-2769-0410-ae09-e727e7b20653>
2009-05-06 09:00:38 +0000

    * Exercise_4 implemented

---
+++ Exercise_4/main.cpp
@@ -0,0 +1,65 @@
+// system includes
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+// global namespace declaration
+using namespace std;
+
+// forward declaration 
+void			EndOfExecPause();
+void			PrintVector(const vector< int >& inVec);
+
+// --------------------------------------------------------------------------
+//		main
+// --------------------------------------------------------------------------
+int main ()
+{
+	int userIn;
+	vector< int > vecIn;
+
+	while ( cin >> userIn )
+	{
+		vecIn.push_back(userIn);
+	}
+
+	cout << "Before sorting: ";
+	PrintVector(vecIn);
+
+	sort(vecIn.begin(),vecIn.end());
+	
+	cout << "After sorting: ";
+	PrintVector(vecIn);
+
+	EndOfExecPause();
+
+	return 0;
+}
+
+// --------------------------------------------------------------------------
+//		PrintVector
+// --------------------------------------------------------------------------
+void
+PrintVector(const vector< int >& inVec)
+{
+	cout << "[ " ;
+	for ( int i=0; i<inVec.size(); ++i)
+		cout << inVec[i] << " ";
+	cout << "]\n";
+	return;
+}
+
+// --------------------------------------------------------------------------
+//		EndOfExecPause
+// --------------------------------------------------------------------------
+void 
+EndOfExecPause()
+{
+	cin.clear();
+	
+	string a;
+	cin >> a;
+	cin >> a;
+}
+
