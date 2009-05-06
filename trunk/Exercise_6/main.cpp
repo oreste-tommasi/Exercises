ae1fc2cd21e05d226c213a8da557fc22f5066325
emiliano <emiliano@1728cb99-2769-0410-ae09-e727e7b20653>
2009-05-06 09:51:01 +0000

    * Exercise_6 implemented

---
+++ trunk/Exercise_6/main.cpp
@@ -0,0 +1,148 @@
+// system includes
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+// global namespace declaration
+using namespace std;
+
+// forward declaration 
+void			EndOfExecPause();
+void			PrintVector(const vector< int >& inVec);
+
+// --------------------------------------------------------------------------
+//		main
+// --------------------------------------------------------------------------
+int main ()
+{
+	int userIn;
+	vector< int > vecIn;
+
+	while ( cin >> userIn )
+	{
+		vecIn.push_back( userIn );
+	}
+
+	PrintVector( vecIn );
+
+	vector< int >::iterator myIter;
+
+	int valueFind;
+
+	cin.clear();
+
+	string a;
+	cin >> a;
+	
+	// Ricerca e swap (RIVEDERE LO SWAP e controllo sul find!!!!!!)
+
+	cin >> valueFind;
+
+	myIter=find(vecIn.begin(),vecIn.end(),valueFind); 
+	//if myIter != vecIn.end()
+	//	swap( vecIn[0] , *myIter ) ???
+
+	int temp; 
+	temp = *myIter;
+	*myIter=vecIn[0];
+	vecIn[0]=temp;
+	PrintVector(vecIn);
+
+	// Inserimento
+
+	cin >> temp;
+	
+	for( myIter=vecIn.begin() ; myIter < vecIn.end() ; ++ myIter)
+	{
+		if ( temp > *myIter )
+		{
+			vecIn.insert((myIter+1),temp);
+			break;
+		}
+			
+	}
+
+	PrintVector(vecIn);
+
+	
+	// Cancellazione
+
+	cin >> temp;
+
+	for( myIter=vecIn.begin() ; myIter < vecIn.end() ; ++ myIter)
+	{
+		if ( temp < *myIter )
+		{
+			vecIn.erase(myIter);
+			break;
+		}
+			
+	}
+
+	PrintVector(vecIn);
+	//cout << myIter;
+
+
+
+	//cout << "\n\nBefore resize:\n\n";
+	//
+	//cout << "Size:\t" << vecIn.size() << "\n"
+	//	 << "Capacity:\t" << vecIn.capacity() << "\n"; 
+
+	//vecIn.resize(0);
+
+ //   
+
+	//cout << "\n\nAfter resize:\n\n";
+	//PrintVector(vecIn);
+	//cout << "Size:\t" << vecIn.size() << "\n"
+	//	 << "Capacity:\t" << vecIn.capacity() << "\n"; 
+
+	//sort(vecIn.begin(),vecIn.end());
+	//
+	//cout << "After sorting: ";
+	//PrintVector(vecIn);
+
+	//cout << "dimensione: " << vecIn.size()
+	//	  << "\n";
+	
+	////PrintVector( vecIn );
+	//
+	//for( myIter=vecIn.begin() ; myIter < (vecIn.end() - 1) ; ++ myIter)
+	//{
+	//	if ( *myIter > *(myIter + 1))
+	//		cout << *myIter;
+	//}
+
+	EndOfExecPause();
+
+	return 0;
+}
+
+// --------------------------------------------------------------------------
+//		PrintVector
+// --------------------------------------------------------------------------
+void
+PrintVector(const vector< int >& inVec)
+{
+	cout << "[ " ;
+	for ( int i=0; i<inVec.size(); ++i)
+		cout << inVec[i] << " ";
+	cout << "]\n";
+	return;
+}
+
+// --------------------------------------------------------------------------
+//		EndOfExecPause
+// --------------------------------------------------------------------------
+void 
+EndOfExecPause()
+{
+	cin.clear();
+	
+	string a;
+	cin >> a;
+	cin >> a;
+}
+
