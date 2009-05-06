62d9aa25b4497b870fb1cbedd3d0f069350e3fe7
emiliano <emiliano@1728cb99-2769-0410-ae09-e727e7b20653>
2009-05-06 10:28:36 +0000

    * Exercise_7 implemented

---
+++ Exercise_7/main.cpp
@@ -0,0 +1,81 @@
+// system includes
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+// global namespace declaration
+using namespace std;
+
+// forward declaration 
+void	EndOfExecPause();
+void	PrintVector( const vector< int >& inVec);
+bool	IsOdd ( int );
+
+
+// --------------------------------------------------------------------------
+//		main
+// --------------------------------------------------------------------------
+int main ()
+{
+	int userIn;
+	vector< int > vecIn;
+
+	while ( cin >> userIn )
+	{
+		vecIn.push_back( userIn );
+	}
+	
+	PrintVector( vecIn );
+
+	vector< int >::iterator myIter;
+	
+	myIter = remove_if( vecIn.begin() , vecIn.end() , IsOdd );
+
+	cout << "\n\n";
+
+	vecIn.erase( myIter, vecIn.end());
+
+	PrintVector( vecIn );
+
+	EndOfExecPause();
+
+	return 0;
+}
+
+// --------------------------------------------------------------------------
+//		PrintVector
+// --------------------------------------------------------------------------
+void
+PrintVector(const vector< int >& inVec)
+{
+	cout << "[ " ;
+	for ( int i=0; i<inVec.size(); ++i)
+		cout << inVec[i] << " ";
+	cout << "]\n";
+	return;
+}
+
+// --------------------------------------------------------------------------
+//		EndOfExecPause
+// --------------------------------------------------------------------------
+void 
+EndOfExecPause()
+{
+	cin.clear();
+	
+	string a;
+	cin >> a;
+	cin >> a;
+}
+
+// --------------------------------------------------------------------------
+//		IsOdd
+// --------------------------------------------------------------------------
+bool
+IsOdd (int i) 
+{ 
+	return ((i%2)==1); 
+}
+
+
