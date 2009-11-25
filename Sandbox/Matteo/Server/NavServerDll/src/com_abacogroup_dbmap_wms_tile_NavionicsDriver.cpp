85f108f1a0ecdfab517f5735ceab09e35f68e600
matteo <matteo@1728cb99-2769-0410-ae09-e727e7b20653>
2009-11-25 09:36:15 +0000

    * Debug version using Navionics libraries. * Prints on raw files.

--- Sandbox/Matteo/Server/NavServerDll/src/com_abacogroup_dbmap_wms_tile_NavionicsDriver.cpp
+++ Sandbox/Matteo/Server/NavServerDll/src/com_abacogroup_dbmap_wms_tile_NavionicsDriver.cpp
@@ -1,6 +1,7 @@
 
 #include "com_abacogroup_dbmap_wms_tile_NavionicsDriver.h"
 #include "NavServer.h"
+#include "malloc.h"
 
 /*
  * Class:     com_abacogroup_dbmap_wms_tile_NavionicsDriver
@@ -8,9 +9,13 @@
  * Signature: (Ljava/lang/String;)V
  */
 JNIEXPORT void JNICALL Java_com_abacogroup_dbmap_wms_tile_NavionicsDriver_init
-  (JNIEnv *, jobject, jstring)
+  (JNIEnv *env, jobject, jstring inFile)
 {
-	NServ_Init();
+	char* fileName=(char *)env->GetStringUTFChars(inFile, 0);
+
+	NServ_Init( fileName );
+
+	env->ReleaseStringUTFChars( inFile, fileName );
 }
 
 /*
@@ -35,22 +40,36 @@ jint JNICALL Java_com_abacogroup_dbmap_wms_tile_NavionicsDriver_getTile
 	theImage.mSize.mHeight	= inSide;
 	theImage.mChannelCount	= 4;
 	theImage.mBuffer			= 0;
+	jsize theSize = inSide*inSide*4;
 
 	// buffer
 	jint			err;
 	jboolean		isVerCopy;
 	jbyte*		imageBuffer = env->GetByteArrayElements( outBA, &isVerCopy );
 
+	//jsize			len  = env->GetArrayLength(outBA); 
+	//if ( len < inSide*inSide*4 )
+	//	return kNSerErr_OutOfMemory;
+	//jbyte*		imageBuffer = (jbyte *)malloc(len * sizeof(jbyte));
+	// env->GetByteArrayRegion( outBA ,0, len, imageBuffer ); 
+
 	if ( imageBuffer )
 	{
 		theImage.mBuffer = (unsigned char*) imageBuffer;
 		err = NServ_GetImage( geoRect, &theImage );
+		FILE* fd = fopen( "C:\\NAV\\test.raw", "wb+");
+		fwrite( imageBuffer, 1, theImage.mChannelCount* theImage.mSize.mWidth * theImage.mSize.mHeight, fd);
+		fclose( fd );
 	}
 	else
 		err = kNSerErr_OutOfMemory;
 
-	if ( isVerCopy )
+	//if ( isVerCopy )
 		env->ReleaseByteArrayElements( outBA, imageBuffer, 0 ); 
+
+	//env->SetByteArrayRegion( outBA, 0, len, imageBuffer);
+	//env->ReleaseByteArrayElements( outBA, imageBuffer, 0 ); 
+	//free( imageBuffer );
 
 	return err;
 }
