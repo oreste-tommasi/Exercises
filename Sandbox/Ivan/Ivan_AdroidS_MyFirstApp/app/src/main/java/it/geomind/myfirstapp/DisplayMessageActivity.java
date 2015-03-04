68693449a8c6fa6a95cb61a7531ea3519d8966e1
ivan <ivan@1728cb99-2769-0410-ae09-e727e7b20653>
2015-03-04 16:59:59 +0000

    * app finalized

--- Sandbox/Ivan/Ivan_AdroidS_MyFirstApp/app/src/main/java/it/geomind/myfirstapp/DisplayMessageActivity.java
+++ Sandbox/Ivan/Ivan_AdroidS_MyFirstApp/app/src/main/java/it/geomind/myfirstapp/DisplayMessageActivity.java
@@ -1,9 +1,11 @@
 package it.geomind.myfirstapp;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class DisplayMessageActivity extends ActionBarActivity {
@@ -11,7 +13,18 @@ public class DisplayMessageActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_display_message);
+
+        // Get the message from the intent
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MyActivity.EXTRA_MESSAGE);
+
+        // Create the text view
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+        // Set the text view as the activity layout
+        setContentView(textView);
     }
 
     @Override
