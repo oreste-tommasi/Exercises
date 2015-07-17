b0063f8ece6357b669a93ceab232a36bdda627e4
gianluca <gianluca@1728cb99-2769-0410-ae09-e727e7b20653>
2015-07-17 16:50:15 +0000

    * sandbox for windows 8.1 dev-tests added

---
+++ Sandbox/windows8/LaunchApp/LaunchApp/EulaPage.xaml.h
@@ -0,0 +1,23 @@
+﻿//
+// EulaPage.xaml.h
+// Declaration of the EulaPage class
+//
+
+#pragma once
+
+#include "EulaPage.g.h"
+
+namespace LaunchApp
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	[Windows::Foundation::Metadata::WebHostHidden]
+	public ref class EulaPage sealed
+	{
+	public:
+		EulaPage();
+	private:
+		void Accepted_ButtonClick(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+	};
+}
